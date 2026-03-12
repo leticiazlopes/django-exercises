8877c09ebc7d02bdac493559f10c7cef0c3e20d2
Leticia Lopes <leticiazlzlima@gmail.com>
2026-03-12 17:58:46 -0300

    add pratica 3

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('home/', views.home, name='blog_home'),
     path('eco/<int:id>/', views.eco, name='blog_eco'),
     path('info/', views.info, name='blog_info'),
+    path('index/', views.index, name='blog_index'),
+    path('contact/<str:phone>/', views.contact, name='blog_contact'),
+    path('about/', views.about, name='blog_about'),
+    
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
+from datetime import date
 
 def home(request):
     return HttpResponse("Welcome to my django blog!")
@@ -14,3 +15,28 @@ def info(request):
      "framework": "Django",
      "semestre": "2025.2"
   })
+
+def index(request):
+
+    servicos = {"nome": "Desenvolvimento Web",
+                "descricao": "Criação de sites e aplicações web personalizadas",
+                "preco": 1500.00}, {"nome": "Consultoria em SEO",
+                "descricao": "Otimização de sites para mecanismos de busca",
+                "preco": 800.00}
+    
+    contexto = {"name": "Leticia",
+                "age": 25,
+                "hobbies": ["coding", "traveling", "cooking"],
+                "city": "São Paulo",
+                "now": date.today(),
+                "is_logged_in": True,
+                "role": "admin",
+                "servicos": servicos
+                }
+    return render(request, 'index.html', contexto)
+
+def contact(request, phone):
+    return HttpResponse(f"Contact us at contato@meublog.com or call {phone}")
+
+def about(request):
+    return render(request, 'home.html')
